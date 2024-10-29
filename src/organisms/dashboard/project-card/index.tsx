b9f8c8209cadd51a5514dc53a923de328c8adc77
anurag-elevano <nitinkumardeshmukh19@gmail.com>
2024-10-29 22:33:57 +0530

    feat: project card added

---
+++ src/organisms/dashboard/project-card/index.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Label,
+} from "../../../atoms";
+
+function ProjectCard() {
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Framework</Label>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button>View All</Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default ProjectCard;
