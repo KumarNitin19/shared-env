e86712bd5a0788c9b5d8f7dd7a76b4cea6b10aa6
anurag-elevano <nitinkumardeshmukh19@gmail.com>
2024-10-29 22:34:12 +0530

    feat: project cards added to dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,19 @@
 import DashboardPageZeroState from "../../organisms/dashboard/dashboard-page-zero-state";
+import ProjectCard from "../../organisms/dashboard/project-card";
 
 const DashboardPage = () => {
   return (
     <div className="h-full">
-      <DashboardPageZeroState />
+      {/* <DashboardPageZeroState /> */}
+      <div className="h-full p-8 flex flex-col items-start gap-4">
+        <h1 className="text-3xl">Dashboard</h1>
+        <span className="text-subtle">All Projects</span>
+        <div className="grid grid-cols-3 w-full gap-6">
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
+      </div>
     </div>
   );
 };
