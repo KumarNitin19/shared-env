cd4899f259bb5123177eb87e144d151d059f06cf
anurag-elevano <nitinkumardeshmukh19@gmail.com>
2024-12-09 23:15:40 +0530

    feat: mui divider component added

--- src/atoms/Divider.tsx
+++ src/atoms/Divider.tsx
@@ -1,28 +1,5 @@
-import * as React from "react";
-import * as SeparatorPrimitive from "@radix-ui/react-separator";
-import { cn } from "../molecules/core/lib/utils";
+import { Divider as MuiDivider, DividerProps } from "@mui/material";
 
-const Divider = React.forwardRef<
-  React.ElementRef<typeof SeparatorPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
->(
-  (
-    { className, orientation = "horizontal", decorative = true, ...props },
-    ref
-  ) => (
-    <SeparatorPrimitive.Root
-      ref={ref}
-      decorative={decorative}
-      orientation={orientation}
-      className={cn(
-        "shrink-0 bg-border",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
-        className
-      )}
-      {...props}
-    />
-  )
-);
-Divider.displayName = SeparatorPrimitive.Root.displayName;
-
-export { Divider };
+export const Divider = (props: DividerProps) => {
+  return <MuiDivider {...props} />;
+};
