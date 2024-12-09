5ceeb5f4abfd85658534027bfd2664ea830b0e79
anurag-elevano <nitinkumardeshmukh19@gmail.com>
2024-12-09 23:01:57 +0530

    fix: styles added for button

--- src/atoms/Button.tsx
+++ src/atoms/Button.tsx
@@ -1,5 +1,28 @@
 import { ButtonProps, Button as MUIButton } from "@mui/material";
 
+const styles = {
+  button: {
+    height: 40,
+    "&.MuiButton-contained": {
+      background: "#3c7c41",
+    },
+    "&.MuiButton-outlined": {
+      color: "#3c7c41",
+      borderColor: "#3c7c41",
+      "&:hover": {
+        backgroundColor: "#3c7c410a",
+      },
+    },
+    "&.MuiButton-text": {
+      paddingY: 0,
+      color: "#3c7c41",
+      "&:hover": {
+        backgroundColor: "#3c7c410a",
+      },
+    },
+  },
+};
+
 export const Button = (props: ButtonProps) => {
-  return <MUIButton {...props} />;
+  return <MUIButton {...props} sx={{ ...styles.button, ...props?.sx }} />;
 };
