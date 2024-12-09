2dc4dcc8e64ab70cc8cba6c09dc57f3c5f5ab98e
anurag-elevano <nitinkumardeshmukh19@gmail.com>
2024-12-09 23:06:55 +0530

    fix: create environment group buutton fixes

--- src/molecules/project/components/Projects/index.tsx
+++ src/molecules/project/components/Projects/index.tsx
@@ -28,7 +28,7 @@ function Projects({ projects }: Props) {
         <Divider orientation="vertical" />
         <CopyText text="78F9A2E7-9C1B-4A8D-AE67-82DF7D1F5C36" />
       </div>
-      <div className="w-full p-6 flex-1 flex flex-col gap-4 overflow-auto bg-card rounded-xl shadow dark:shadow-[#3b3c45]">
+      <div className="w-full p-4 flex-1 flex flex-col gap-4 overflow-auto bg-card rounded-xl shadow dark:shadow-[#3b3c45]">
         {/* <ZeroState /> */}
         <EnvironmentVariableGroupList />
       </div>
--- src/organisms/projects/environment-variable-group-list/index.tsx
+++ src/organisms/projects/environment-variable-group-list/index.tsx
@@ -1,14 +1,24 @@
 import { Button, Icon } from "../../../atoms";
 import AddEnvironmentGroup from "../../../molecules/add-environment-group";
 
+const styles = {
+  createEnvironmentGroupButton: {
+    height: "fit-contetn",
+  },
+};
+
 function EnvironmentVariableGroupList() {
   return (
     <div className="grid row-gap-6">
       <div className="flex items-center justify-between">
         <span className="font-bold">Environment Groups</span>
         <AddEnvironmentGroup>
-          <Button variant="text" className="p-0 h-fit">
-            <Icon icon="fluent:add-16-regular" className="h-5 w-5" />
+          <Button
+            variant="text"
+            startIcon={
+              <Icon icon="fluent:add-16-regular" className="h-5 w-5" />
+            }
+            sx={styles.createEnvironmentGroupButton}>
             Create Environment Group
           </Button>
         </AddEnvironmentGroup>
