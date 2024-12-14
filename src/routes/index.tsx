9c65f2750380ea608684b7853454eaeed9870f0a
anurag-elevano <nitinkumardeshmukh19@gmail.com>
2024-12-15 00:25:49 +0530

    fix: dashboard container added to route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,9 +3,9 @@ import PrivateRoute from "./privateRoute";
 import PublicRoute from "./publicRoute";
 import Container from "../molecules/layout/components/Container";
 import PageNotFound from "../molecules/layout/components/PageNotFound";
-import DashboardPage from "../pages/dashboard";
 import ProjectPage from "../pages/project";
 import { LoginContainer } from "../containers/login-container";
+import { DashboardContainer } from "../containers/dashboard-container";
 
 const router = createBrowserRouter([
   {
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
         path: "/",
         element: (
           <PrivateRoute>
-            <DashboardPage />
+            <DashboardContainer />
           </PrivateRoute>
         ),
       },
