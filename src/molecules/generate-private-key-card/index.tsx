2a9457d3c657781f538ee4b237c480ca7ec35b18
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-01-11 22:50:39 +0530

    fix: generate private key card fixes

--- src/molecules/generate-private-key-card/index.tsx
+++ src/molecules/generate-private-key-card/index.tsx
@@ -4,7 +4,8 @@ import { Box } from "../../atoms/Box";
 import { useThemeToggle } from "../../hooks/useThemeToggle";
 import { Typography } from "../../atoms/Typography";
 import { useTheme } from "@mui/material";
-import { Button } from "../../atoms";
+import { Button, Icon } from "../../atoms";
+import { useCallback, useState } from "react";
 
 const styles = {
   signInCard: (theme: string) => ({
@@ -14,11 +15,25 @@ const styles = {
     backgroundSize: "cover",
     backdropFilter: "blur(62px)",
   }),
+  generateApiKeyButton: {
+    "&.Mui-disabled": {
+      color: "#fff",
+    },
+  },
 };
 
 const GeneratePrivateKeyCard = () => {
+  const [isGeneratingKey, setIsGeneratingKey] = useState<boolean>(false);
   const theme = useTheme();
   const { mode } = useThemeToggle();
+
+  const generateKey = useCallback(() => {
+    setIsGeneratingKey(true);
+    setTimeout(() => {
+      setIsGeneratingKey(false);
+    }, 2000);
+  }, []);
+
   return (
     <Box
       display="flex"
@@ -29,13 +44,13 @@ const GeneratePrivateKeyCard = () => {
       justifyContent="center"
       gap={6}
       sx={styles.signInCard(mode)}>
-      <Box display="flex" flexDirection="column" gap={4}>
+      <Box display="flex" textAlign="center" flexDirection="column" gap={4}>
         <Typography
           variant="h4"
           fontSize={32}
           fontWeight={500}
           color={theme.palette.surface100.main}>
-          Hey, Nitin! Welcome to VarVault
+          Hey, Nitin! <br /> Welcome to VarVault
         </Typography>
         <Typography
           textAlign="center"
@@ -44,7 +59,16 @@ const GeneratePrivateKeyCard = () => {
           Generate your unique private key
         </Typography>
       </Box>
-      <Button variant="contained">Generate Private Key</Button>
+      <Button
+        variant="contained"
+        startIcon={
+          isGeneratingKey ? <Icon icon="line-md:loading-twotone-loop" /> : null
+        }
+        disabled={isGeneratingKey}
+        onClick={generateKey}
+        sx={styles.generateApiKeyButton}>
+        {isGeneratingKey ? "Generating..." : "Generate Private Key"}
+      </Button>
     </Box>
   );
 };
