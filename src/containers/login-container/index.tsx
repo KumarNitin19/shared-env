dbc8cb46e7b06e8a5e7630fd6a7b473f353c6ae4
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-01-16 23:52:33 +0530

    fix: signin component fixes

--- src/containers/login-container/index.tsx
+++ src/containers/login-container/index.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import SignIn from "../../molecules/auth/signin";
 import { useToast } from "../../hooks/use-toast";
 import useLocalStorage from "../../hooks/useLocalStorage";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { signInWithPopup } from "firebase/auth";
 import { auth, googleAuthProvider } from "../../molecules/auth/utils/firebase";
 
 export const LoginContainer = () => {
@@ -13,16 +13,15 @@ export const LoginContainer = () => {
   const { setItem } = useLocalStorage();
 
   const signInWithGooglePopup = () => {
-    console.log("clicked");
     signInWithPopup(auth, googleAuthProvider)
-      .then((result) => {
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential?.accessToken;
+      .then(async (result) => {
         const user = result.user;
+        const token = await user.getIdToken();
+        console.log(token);
         setItem(
           "userDetails",
           JSON.stringify({
-            access_token: user.getIdToken(),
+            access_token: token,
             email: user.email,
             display_name: user.displayName,
             profile_image: user.photoURL,
@@ -37,10 +36,6 @@ export const LoginContainer = () => {
         toast({
           description: `Something went wrong, please try again.`,
         });
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        // const email = error.customData.email;
-        // const credential = GoogleAuthProvider.credentialFromError(error);
       });
   };
   return <SignIn onSignUp={signInWithGooglePopup} />;
