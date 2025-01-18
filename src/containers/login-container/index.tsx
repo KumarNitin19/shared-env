f73084247ac7e894bdaa2e82044c66335d035d5d
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-01-18 21:21:42 +0530

    chore: toaster removed

--- src/containers/login-container/index.tsx
+++ src/containers/login-container/index.tsx
@@ -1,14 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import SignIn from "../../molecules/auth/signin";
-import { useToast } from "../../hooks/use-toast";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import { signInWithPopup } from "firebase/auth";
 import { auth, googleAuthProvider } from "../../molecules/auth/utils/firebase";
 
 export const LoginContainer = () => {
   const navigate = useNavigate();
-
-  const { toast } = useToast();
 
   const { setItem } = useLocalStorage();
 
@@ -27,16 +24,10 @@ export const LoginContainer = () => {
             profile_image: user.photoURL,
           })
         );
-        toast({
-          description: `${user.displayName} logged in successfully.`,
-        });
+
         navigate("/");
       })
-      .catch((_error) => {
-        toast({
-          description: `Something went wrong, please try again.`,
-        });
-      });
+      .catch((_error) => {});
   };
   return <SignIn onSignUp={signInWithGooglePopup} />;
 };
--- src/hooks/useCopyToClipboard.tsx
+++ src/hooks/useCopyToClipboard.tsx
@@ -1,9 +1,7 @@
 import { useCallback, useState } from "react";
-import { useToast } from "./use-toast";
 
 export default function useCopyToClipboard() {
   const [isCopied, setIsCopied] = useState<boolean>(false);
-  const { toast, dismiss } = useToast();
 
   const copy = useCallback((text: string) => {
     if (text) {
@@ -12,12 +10,7 @@ export default function useCopyToClipboard() {
       const timeout = setTimeout(() => {
         setIsCopied(false);
         clearTimeout(timeout);
-        dismiss();
       }, 1500);
-
-      toast({
-        description: `Copied to clipboard.`,
-      });
     }
   }, []);
 
