0dcf51cce92ee816781cb3eee1096da1fc19fd5f
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-01-19 21:56:58 +0530

    chore: add environment group query added

--- src/query/evironmentGroupQuery.ts
+++ src/query/evironmentGroupQuery.ts
@@ -1,10 +1,29 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { privateApiClient } from "../utils/apiUtils";
 
-export function useEnvironmentGroup() {
-  const url = "/environment-group/";
+export function useEnvironmentGroup(projectId: string) {
+  const url = `/${projectId}/environment-group/`;
   return useQuery({
     queryKey: ["environmentGroup"],
     queryFn: async () => privateApiClient({ url }) as Promise<unknown>,
+  });
+}
+
+export function useAddEnvironmentGroup() {
+  return useMutation({
+    mutationFn: async ({
+      projectId,
+      environmentGroupData,
+    }: {
+      projectId: string;
+      environmentGroupData: unknown;
+    }) => {
+      const url = `/add/environment-group/${projectId}`;
+      return privateApiClient({
+        url,
+        method: "POST",
+        data: environmentGroupData,
+      }) as Promise<unknown>;
+    },
   });
 }
