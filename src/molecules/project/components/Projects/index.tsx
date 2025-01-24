242f64bb1ec064a8c7d0e46482092b1b996bb88b
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-01-25 03:09:21 +0530

    chore: share button added to invite users

--- src/molecules/project/components/Projects/index.tsx
+++ src/molecules/project/components/Projects/index.tsx
@@ -7,6 +7,8 @@ import CopyText from "../../../copy-text";
 import { Box } from "../../../../atoms/Box";
 // import ProjectPageZeroState from "../../../../organisms/projects/project-page-zero-state";
 import { Divider } from "../../../../atoms/Divider";
+import { Button } from "../../../../atoms/Button";
+import { Icon } from "../../../../atoms/Icon";
 
 type Props = {
   projects: {
@@ -24,21 +26,24 @@ function Projects({ projects }: Props) {
       gap={2.5}
       height="100%"
       width="100%">
-      <Box display="grid" rowGap={1}>
-        <Typography
-          variant="h4"
-          fontSize={32}
-          fontWeight={400}
-          color={palette.surface100.main}>
-          {projectName}
-        </Typography>
-        <Box display="flex" gap={1.25} alignItems="center">
-          <Typography color={palette.surface80.main} variant="subtitle1">
-            78F9A2E7-9C1B-4A8D-AE67-82DF7D1F5C36
+      <Box display="flex" alignItems="center" justifyContent="space-between">
+        <Box display="grid" rowGap={1}>
+          <Typography
+            variant="h4"
+            fontSize={32}
+            fontWeight={400}
+            color={palette.surface100.main}>
+            {projectName}
           </Typography>
-          <Divider orientation="vertical" sx={{ height: 12 }} />
-          <CopyText text="78F9A2E7-9C1B-4A8D-AE67-82DF7D1F5C36" />
+          <Box display="flex" gap={1.25} alignItems="center">
+            <Typography color={palette.surface80.main} variant="subtitle1">
+              78F9A2E7-9C1B-4A8D-AE67-82DF7D1F5C36
+            </Typography>
+            <Divider orientation="vertical" sx={{ height: 12 }} />
+            <CopyText text="78F9A2E7-9C1B-4A8D-AE67-82DF7D1F5C36" />
+          </Box>
         </Box>
+        <Button startIcon={<Icon icon="material-symbols:add" />}>Share</Button>
       </Box>
       <Box
         flex={1}
