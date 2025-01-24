cb97d6e8d4272739c22116cc78815eb0e6cb9761
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-01-24 20:23:02 +0530

    chore: invite github user flow

--- src/utils/commonUtils.ts
+++ src/utils/commonUtils.ts
@@ -1,3 +1,23 @@
 export function generateUID() {
   return Date.now().toString(36) + Math.random().toString(36).substr(2);
 }
+
+// flow to inivte people on github
+// const inviteToRepo = async (repoOwner, repoName, username, githubToken) => {
+//   const url = `https://api.github.com/repos/${repoOwner}/${repoName}/collaborators/${username}`;
+//   try {
+//     const response = await axios.put(
+//       url,
+//       {},
+//       {
+//         headers: {
+//           Authorization: `token ${githubToken}`,
+//           Accept: "application/vnd.github.v3+json",
+//         },
+//       }
+//     );
+//     console.log("Invitation sent:", response.data);
+//   } catch (error) {
+//     console.error("Error sending invitation:", error);
+//   }
+// };
