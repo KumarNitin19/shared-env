987e242e27a6385a31066671b3187c68cbd4c791
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-01-27 22:22:40 +0530

    fix: add project api integrated

--- src/containers/dashboard-container/index.tsx
+++ src/containers/dashboard-container/index.tsx
@@ -1,7 +1,62 @@
+import { useCallback, useState } from "react";
 import DashboardPage from "../../pages/dashboard";
+import { AddProjectType } from "../../types/project.type";
+import { useAddProject, useProjects } from "../../query/projectQuery";
+import AddProject from "../../molecules/add-project";
+import useSnackbar from "../../hooks/useSnackbar";
+import Loader from "../../molecules/loader";
 
 const DashboardContainer = () => {
-  return <DashboardPage />;
+  const [isAddProject, setIsAddProject] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { mutateAsync: addProject } = useAddProject();
+  const { refetch } = useProjects();
+  const { addAlert } = useSnackbar();
+
+  const handleOpenAddProject = useCallback(() => setIsAddProject(true), []);
+
+  const handleCloseAddProject = useCallback(() => setIsAddProject(false), []);
+
+  const onAddProject = useCallback(
+    async (body: AddProjectType) => {
+      setIsLoading(true);
+      try {
+        const res = await addProject(body);
+
+        if (res?.projectId) {
+          refetch();
+          addAlert({
+            message: "Project created successfully!!",
+            variant: "filled",
+            type: "success",
+          });
+        }
+      } catch (error) {
+        console.log(error);
+        addAlert({
+          message: "Something went wrong, please try again!",
+          variant: "filled",
+          type: "error",
+        });
+      } finally {
+        setIsLoading(false);
+        handleCloseAddProject();
+      }
+    },
+    [addAlert, refetch, handleCloseAddProject]
+  );
+
+  return (
+    <>
+      <DashboardPage handleOpenAddProject={handleOpenAddProject} />
+      <AddProject
+        open={isAddProject}
+        onClose={handleCloseAddProject}
+        handleAddProject={onAddProject}
+      />
+      <Loader loader={isLoading} fullPage />
+    </>
+  );
 };
 
 export default DashboardContainer;
