eda5b5b9c04d4dedd4d0824dcd42d21e54c37d57
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-01-28 21:33:15 +0530

    feat: confirmation dialog compoent created

---
+++ src/molecules/confirmation-dialog/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "../../atoms/Dialog";
+import { Typography } from "../../atoms/Typography";
+import { Button } from "../../atoms/Button";
+
+type ComponentProps = {
+  open: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  children: React.ReactNode;
+  label: string;
+};
+
+const ConfirmationDialog: React.FC<ComponentProps> = ({
+  open = false,
+  onClose = () => {},
+  onConfirm = () => {},
+  label = "Confirm Dialog",
+  children,
+}) => {
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>
+        <Typography>{label}</Typography>
+      </DialogTitle>
+      <DialogContent>{children}</DialogContent>
+      <DialogActions>
+        <Button variant="outlined" onClick={onClose}>
+          Cancel
+        </Button>
+        <Button variant="contained" onClick={onConfirm}>
+          Confirm
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ConfirmationDialog;
