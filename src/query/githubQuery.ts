995620d94f6e9911357a1924d5c83e02a2ce5c4b
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-02-08 14:01:43 +0530

    feat: link github repo query added

--- src/query/githubQuery.ts
+++ src/query/githubQuery.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { privateApiClient } from "../utils/apiUtils";
 import { GithubRepos } from "../types/project.type";
 
@@ -14,6 +14,42 @@ export const useGithubRepos = (githubAccessToken: string) => {
             githubAccessToken: githubAccessToken,
           },
         });
+        return res.data;
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    },
+  });
+};
+
+export const useLinkGithubRepos = () => {
+  return useMutation({
+    mutationKey: ["link-github-repo"],
+    mutationFn: async ({
+      githubAccessToken,
+      projectId,
+      githubRepo,
+    }: {
+      githubAccessToken: string;
+      projectId: string;
+      githubRepo: string;
+    }): Promise<{
+      message: string;
+      githubRepo: string;
+      members: string[];
+    }> => {
+      try {
+        const url = "/link-github-repo/";
+        const res = await privateApiClient({
+          url,
+          method: "POST",
+          params: {
+            githubAccessToken,
+            projectId,
+          },
+          data: { githubRepo },
+        });
+        console.log(res?.data);
         return res.data;
       } catch (error) {
         return Promise.reject(error);
