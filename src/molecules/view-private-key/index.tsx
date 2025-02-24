8e469bd123a19fe205be56207fade465c8e96eb0
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-02-24 13:00:17 +0530

    fix: all static images are remove and component wrapped with cardwithgradientborder component

--- src/molecules/view-private-key/index.tsx
+++ src/molecules/view-private-key/index.tsx
@@ -4,13 +4,12 @@ import Dialog, { DialogContent } from "../../atoms/Dialog";
 import { Box } from "../../atoms/Box";
 import { Typography } from "../../atoms/Typography";
 import CopyText from "../copy-text";
-import CARD_BACKGROUND_LIGHT from "../../assets/images/sign-in-card-bg.svg";
-import CARD_BACKGROUND_DARK from "../../assets/images/sign-in-card-bg-dark.svg";
 import { useThemeToggle } from "../../hooks/useThemeToggle";
 import { Icon } from "../../atoms/Icon";
 import { ThemeEnum } from "../../providers/ThemeProvider";
 import { useEffect, useState } from "react";
 import useUser from "../../hooks/useUser";
+import CardWithGradientBorder from "../card-with-gradient-border";
 
 const styles = {
   dialog: (theme: string) => ({
@@ -19,16 +18,8 @@ const styles = {
         theme === "light" ? "rgba(255, 255, 255, 0.5)" : "rgba(0, 0, 0, 0.5)",
     },
     "& .MuiPaper-root": {
-      minWidth: 566,
-      width: 566,
-      backgroundColor: theme === "light" ? "#f2f2f2" : "unset",
-      backgroundImage: `url(${
-        theme === "light" ? CARD_BACKGROUND_LIGHT : CARD_BACKGROUND_DARK
-      })`,
-      backdropFilter: "blur(62px)",
-      backgroundSize: "cover",
-      minHeight: 348,
       borderRadius: 6,
+      background: "transparent",
     },
   }),
 
@@ -64,53 +55,55 @@ const ViewPrivateKey = ({ open = false, onClose = () => {} }: Props) => {
 
   return (
     <Dialog open={open} sx={styles.dialog(mode)}>
-      <IconButton onClick={onClose} sx={styles.closeButton}>
-        <Icon
-          icon="material-symbols:close-rounded"
-          color={theme.palette.surface100.main}
-        />
-      </IconButton>
-      <DialogContent sx={styles.dialogContent}>
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          textAlign="center"
-          gap={4}>
-          <Typography
-            title="Private Key!"
-            variant="subtitle2"
-            fontSize={32}
-            color={theme.palette.surface100.main}>
-            Private Key!
-          </Typography>
-          <Typography
-            variant="body2"
-            fontSize={18}
-            color={theme.palette.surface80.main}>
-            Here’s your unique private key,
-            <br /> keep it safe.
-          </Typography>
-        </Box>
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="space-between"
-          flex={1}
-          borderRadius={2}
-          py={2}
-          px={3}
-          bgcolor={mode === ThemeEnum.LIGHT ? "#fff" : "#000"}>
-          <Typography
-            variant="subtitle2"
+      <CardWithGradientBorder>
+        <IconButton onClick={onClose} sx={styles.closeButton}>
+          <Icon
+            icon="material-symbols:close-rounded"
+            color={theme.palette.surface100.main}
+          />
+        </IconButton>
+        <DialogContent sx={styles.dialogContent}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            textAlign="center"
+            gap={4}>
+            <Typography
+              title="Private Key!"
+              variant="subtitle2"
+              fontSize={32}
+              color={theme.palette.surface100.main}>
+              Private Key!
+            </Typography>
+            <Typography
+              variant="body2"
+              fontSize={18}
+              color={theme.palette.surface80.main}>
+              Here’s your unique private key,
+              <br /> keep it safe.
+            </Typography>
+          </Box>
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
             flex={1}
-            fontSize={20}
-            color={theme.palette.surface100.main}>
-            {privateKey}
-          </Typography>
-          <CopyText text={privateKey} fontSize={20} />
-        </Box>
-      </DialogContent>
+            borderRadius={2}
+            py={2}
+            px={3}
+            bgcolor={mode === ThemeEnum.LIGHT ? "#fff" : "#000"}>
+            <Typography
+              variant="subtitle2"
+              flex={1}
+              fontSize={20}
+              color={theme.palette.surface100.main}>
+              {privateKey}
+            </Typography>
+            <CopyText text={privateKey} fontSize={20} />
+          </Box>
+        </DialogContent>
+      </CardWithGradientBorder>
     </Dialog>
   );
 };
